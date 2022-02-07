f17c2d962cadf243929145df94a588cc1d5a236a
Vivian Sung <vivian.sung@ufl.edu>
2022-02-07 00:29:42 -0500

    add files

---
+++ Assets/Scripts/RaycastScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 50f, Color.red);
+
+        if (Input.GetKeyDown("space") && Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, Mathf.Infinity))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.up) * 10f, Color.red);
+            hit.collider.gameObject.GetComponent<TargetScript>().IsHit();
+            Debug.Log("hit");
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+    [SerializeField] Material hit;
+    [SerializeField] Material notHit;
+    private bool _isHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isHit)
+        {
+            GetComponent<Renderer>().material = hit;
+        }
+        //else
+        //{
+        //    GetComponent<Renderer>().material = notHit;
+        //}
+    }
+
+    public void IsHit()
+    {
+        _isHit = true;
+    }
+}
